20927d5fad30ec79dde2213c464a08106bf0146a
Richard Truong <rqtruong@uci.edu>
2020-08-15 22:55:34 -0700

    reload page on initial render, end of lesson 54

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
       }
     });
   } else {
-    res.send('<p>Awaiting compilation...</p>');
+    res.send('<p>Awaiting compilation...</p><script src="/reload/reload.js"></script>');
   }
 });
 
@@ -53,7 +53,11 @@ if (process.env.NODE_ENV === 'development') {
   const reloadServer = reload(app);
   require('./webpack-dev-middleware').init(app);
   require('./webpack-server-compiler').init(function(bundle) {
+    let needsReload = (renderer === undefined);
     renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+    if (needsReload) {
+      reloadServer.reload();
+    }
   })
 }
 
