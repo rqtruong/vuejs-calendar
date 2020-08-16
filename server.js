2b9b0a580861bd98799ca08c07fd262983fc060f
Richard Truong <rqtruong@uci.edu>
2020-08-15 22:33:45 -0700

    create server entry file, end of lesson 50

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
   const reloadServer = reload(app);
   require('./webpack-dev-middleware').init(app);
+  require('./webpack-server-compiler').init(function(bundle) {
+    console.log('Node bundle built');
+  })
 }
 
 server.listen(process.env.PORT, function () {
