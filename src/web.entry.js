20bd67ba3abcb93b5ee83d5d6e77f0a7f92d4bec
Richard Truong <rqtruong@uci.edu>
2020-08-15 22:27:15 -0700

    extract logic from web.entry.js to entry.js, end of lesson 49

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,30 +1,14 @@
-import Vue from 'vue';
 import './style.scss';
-
-import store from './store';
-
 import moment from 'moment-timezone';
 moment.tz.setDefault('UTC');
-Object.defineProperty(Vue.prototype, '$moment', {get() {return this.$root.moment}});
-
-import App from './components/App.vue';
 
 let events = window.__INITIAL_STATE__.map(event=> {
   return {
     description: event.description,
     date: moment(event.date)
   }
-})
-let initialState = Object.assign({}, store.state, {events});
-store.replaceState(initialState);
-
-new Vue({
-  el: '#app',
-  data: {
-    moment
-  },
-  components: { 
-    App
-  },
-  store
 });
+
+import VueCalendar from './entry';
+VueCalendar(events).$mount('#app');
+
